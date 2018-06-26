43671fc554fb44aefce5424c240101789a530b19
SandeepThakare <sandythakare9@gmail.com>
2018-06-26 13:14:16 +0530

    google auth done

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
-export const googleClientID = '255068697322-1pmonksr36vs7mub2pkkinabfn8u5tnf.apps.googleusercontent.com';
-export const googleClientSecret = '6_xNfw9l1VXfyjsuvHg3ivpy';
+export const googleClientID = '';
+export const googleClientSecret = '';
 export const mongoURI = 'mongodb://localhost:27017/blog_everyone';
 export const cookieKey = '123123123';
