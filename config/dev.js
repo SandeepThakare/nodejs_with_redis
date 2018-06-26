c86c1ee505e102de54189ab59871a091534a7b02
SandeepThakare <sandythakare9@gmail.com>
2018-06-26 15:18:47 +0530

    cred

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
-export const googleClientID = '502093955247-lqklp98hvb3jkvi81p6ao5gpnmsg48l7.apps.googleusercontent.com';
-export const googleClientSecret = 'MW1yS57-ijgtYXTkCUWJdT7Q';
+export const googleClientID = '';
+export const googleClientSecret = '';
 export const mongoURI = 'mongodb://localhost:27017/blog_everyone';
 export const cookieKey = '123123123';
